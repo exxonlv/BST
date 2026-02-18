8489dfbce69b599059e3c6897e79b6f048751c98
Ivo <ivomezits@gmail.com>
2026-02-18 17:39:31 +0200

    v3

--- recv.c
+++ recv.c
@@ -1,72 +1,30 @@
 #include "stdmansos.h"
-#include <lib/codec/crc.h>
-#include <string.h>
 
-#define LIGHTRADIO_GROUP 0x2026
-#define LIGHTRADIO_VERSION 1
-#define LIGHTRADIO_TYPE_LIGHT 1
+static uint8_t radioBuffer[RADIO_MAX_PACKET];
 
-typedef struct __attribute__((packed)) {
-    uint8_t magic[4];     // "LRAD"
-    uint8_t version;
-    uint8_t type;
-    uint16_t group;
-    uint16_t sender;
-    uint32_t seq;
-    uint16_t light;
-    uint16_t crc;
-} LightRadioPacket;
-
-static volatile bool havePkt;
-static LightRadioPacket lastPkt;
-static int8_t lastRssi;
-
-static bool magicOk(const uint8_t magic[4])
+static void recvRadio(void)
 {
-    return magic[0] == 'L' && magic[1] == 'R' && magic[2] == 'A' && magic[3] == 'D';
-}
+    int16_t len;
 
-static uint16_t packetCrc(const LightRadioPacket *p)
-{
-    return crc16((const uint8_t *)p, sizeof(*p) - sizeof(p->crc));
-}
-
-static void recvLight(void)
-{
-    uint8_t buffer[RADIO_MAX_PACKET];
-    int16_t len = radioRecv(buffer, sizeof(buffer));
-    if (len != (int16_t)sizeof(LightRadioPacket)) {
-        return;
-    }
-
-    LightRadioPacket pkt;
-    memcpy(&pkt, buffer, sizeof(pkt));
-
-    if (!magicOk(pkt.magic)) return;
-    if (pkt.version != LIGHTRADIO_VERSION) return;
-    if (pkt.type != LIGHTRADIO_TYPE_LIGHT) return;
-    if (pkt.group != LIGHTRADIO_GROUP) return;
-    if (pkt.crc != packetCrc(&pkt)) return;
-
-    lastRssi = radioGetLastRSSI();
-    lastPkt = pkt;
-    havePkt = true;
     greenLedToggle();
+    len = radioRecv(radioBuffer, sizeof(radioBuffer));
+    if (len < 0) {
+        PRINTF("radio receive failed\n");
+    }
+    else if (len > 0) {
+        PRINTF("radio receive %d bytes\n", len);
+        debugHexdump((uint8_t *)radioBuffer, len);
+    }
 }
 
 void appMain(void)
 {
-    radioInit();
-    radioSetReceiveHandle(recvLight);
+    radioSetReceiveHandle(recvRadio);
     radioOn();
 
+    PRINTF("Receiver started...\n");
+
     while (1) {
-        if (havePkt) {
-            havePkt = false;
-            PRINTF("from=%#04x seq=%lu light=%u rssi=%d\n",
-                   lastPkt.sender, (unsigned long)lastPkt.seq,
-                   (unsigned)lastPkt.light, (int)lastRssi);
-        }
         mdelay(1000);
         blueLedToggle();
     }
--- send.c
+++ send.c
@@ -1,65 +1,34 @@
 #include "stdmansos.h"
-#include <light.h>
-#include <lib/codec/crc.h>
-#include <string.h>
-#include <net/address.h>
 
-MosShortAddr localAddress __attribute__((weak)) = 0x0002;
+#define HELLO "Hello world"
 
-#define LIGHTRADIO_GROUP 0x2026
-#define LIGHTRADIO_VERSION 1
-#define LIGHTRADIO_TYPE_LIGHT 1
+#define SERIAL_BUF_LEN 250
+static char serialBuffer[SERIAL_BUF_LEN];
 
-typedef struct __attribute__((packed)) {
-    uint8_t magic[4];     // "LRAD"
-    uint8_t version;
-    uint8_t type;
-    uint16_t group;
-    uint16_t sender;
-    uint32_t seq;
-    uint16_t light;
-    uint16_t crc;
-} LightRadioPacket;
-
-static void fillMagic(uint8_t magic[4])
+static void recvSerial(uint8_t length)
 {
-    magic[0] = 'L';
-    magic[1] = 'R';
-    magic[2] = 'A';
-    magic[3] = 'D';
-}
-
-static uint16_t packetCrc(const LightRadioPacket *p)
-{
-    return crc16((const uint8_t *)p, sizeof(*p) - sizeof(p->crc));
+    if (length >= SERIAL_BUF_LEN) {
+        length = SERIAL_BUF_LEN - 1;
+    }
+    serialBuffer[length] = '\0';
+    PRINTF("%s", serialBuffer);
+    if (serialBuffer[length - 1] != '\n') {
+        PRINTF("\n");
+    }
+    radioSend(serialBuffer, length);
 }
 
 void appMain(void)
 {
-    LightRadioPacket pkt;
-    uint32_t seq = 0;
-
-    memset(&pkt, 0, sizeof(pkt));
-    fillMagic(pkt.magic);
-    pkt.version = LIGHTRADIO_VERSION;
-    pkt.type = LIGHTRADIO_TYPE_LIGHT;
-    pkt.group = LIGHTRADIO_GROUP;
-    pkt.sender = localAddress;
-
-    lightInit();
-    lightOn();
+    serialSetPacketReceiveHandle(PRINTF_SERIAL_ID, recvSerial,
+            (uint8_t *)serialBuffer, sizeof(serialBuffer));
 
     radioOn();
 
+    PRINTF("Sender started...\n");
+
     while (1) {
-        uint16_t light = lightRead();
-
-        pkt.seq = seq++;
-        pkt.light = light;
-        pkt.crc = packetCrc(&pkt);
-		PRINTF("sending light=%u seq=%lu\n", (unsigned)light, (unsigned long)pkt.seq);
-        radioSend(&pkt, sizeof(pkt));
-
+        radioSend(HELLO, sizeof(HELLO));
         redLedToggle();
         mdelay(1000);
     }
